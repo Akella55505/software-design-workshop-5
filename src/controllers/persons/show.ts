777ed755dfb7a21a331d963eb9ddb31cfeed6f2b
Akella <leosaz0617@gmail.com>
2025-10-14 12:53:27 +0300

    fix: assigned proper parameter

--- src/controllers/persons/show.ts
+++ src/controllers/persons/show.ts
@@ -6,7 +6,7 @@ import { PersonService } from '../../services/PersonService';
 export const show = async (req: Request, res: Response, next: NextFunction) => {
   const personService = new PersonService();
   try {
-    const person = await personService.show(req.body);
+    const person = await personService.show(req.params.id);
     res.customSuccess(200, 'Person found', new PersonDto(person));
   } catch (err) {
     return next(err);
